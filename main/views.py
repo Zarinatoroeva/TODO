413e0124cb5489824e5242a2c96558dfba4e691a
Zarina Toroeva <zarinatoroeva@Zarinas-MacBook-Air.local>
2021-01-12 22:14:00 +0600

    copied index.html and styles

--- main/views.py
+++ main/views.py
@@ -5,4 +5,10 @@ def homepage(requests):
 
 def test(requests):
     return render(requests, "test.html")
+
+def second(requests):
+    return HttpResponse("test 2 page")
+
+def third(requests):
+    return HttpResponse("This is page test 3")
     
